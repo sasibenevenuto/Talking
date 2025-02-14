111bf81f6b0dad531852b094e540ead517f36510
Samuel Apolion Benevenuto <sasibenevenuto@gmail.com>
2025-02-14 17:43:03 -0300

    validação da especificação de domínio, projeto de testes

---
+++ src/Ambev.DeveloperEvaluation.Domain/Specifications/ActiveSaleSpecification.cs
@@ -0,0 +1,39 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Specifications
+{
+    public class ActiveSaleSpecification : ISaleSpecification<Sale>
+    {
+        string returnMessage = string.Empty;
+        public string DiscountSpecification(Sale sale)
+        {
+            foreach (var product in sale.Products)
+            {
+                int item = product.Amount;
+                
+                switch (item)
+                {                    
+                    case < 4:
+                        product.Discount = 0;
+                        break;
+                    case < 10:
+                        product.Discount = 10;
+                        break;
+                    case <= 20:
+                        product.Discount = 20;
+                        break;
+                    case > 20:
+                        returnMessage = "It's not possible to sell above 20 identical items";
+                        break;
+                }
+              
+            }
+            return returnMessage;
+        }
+    }
+}
---
+++ tests/Ambev.DeveloperEvaluation.Unit/Domain/Specifications/TestData/ActiveSaleSpecificationTestData.cs
@@ -0,0 +1,66 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Unit.Domain.Specifications.TestData
+{
+
+    /// <summary>
+    /// Provides methods for generating test data using the Bogus library.
+    /// This class centralizes all test data generation to ensure consistency
+    /// across test cases and provide both valid and invalid data scenarios.
+    /// </summary>
+    public static class ActiveSaleSpecificationTestData
+    {
+        public static int Amount { get; set; }
+        /// <summary>
+        /// Configures the Faker to generate valid User entities.
+        /// The generated users will have valid:
+        /// - DateSale (valid format)
+        /// - Customer (valid not null)
+        /// - TotaSaleAmount (valid format)
+        /// - Branch (valid format)
+        /// - Cancelled (valid format)        /// </summary>
+        //private static readonly Faker<Sale> SaleFaker = new Faker<Sale>()
+        //    .RuleFor(u => u.DateSale, f => f.DateTimeReference)
+        //    .RuleFor(u => u.Customer, f => f.Lorem.Word())
+        //    .RuleFor(u => u.TotaSaleAmount, f => f.PickRandom(0m, decimal.MaxValue))
+        //    .RuleFor(u => u.Branch, f => f.Lorem.Word())
+        //    .RuleFor(u => u.Cancelled, f => f.PickRandom(true, false));
+
+        private static readonly Faker<Sale> SaleFaker = new Faker<Sale>()
+                .CustomInstantiator(f => new Sale
+                {
+                    DateSale = DateTime.Now,
+                    Customer = $"Teste Customer",
+                    TotaSaleAmount = 20m,
+                    Branch = "",
+                    Cancelled = false,
+                    Products = new List<Product> { new Product() {
+                        Amount = Amount,
+                        UnitPrice = 1m,
+                        Discount = 0,
+                        TotalAmount = 1m,
+                        Cancelled = false
+                    } }
+                });
+
+        /// <summary>
+        /// Generates a valid Sale entity with randomized data.
+        /// The generated sale will have all properties populated with valid values
+        /// that meet the system's validation requirements.
+        /// </summary>
+        /// <returns>A valid User entity with randomly generated data.</returns>
+        public static Sale GenerateSale(int amount)
+        {
+            Amount = amount;
+            var sale = SaleFaker.Generate();
+            return sale;
+        }
+    }
+}
